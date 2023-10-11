2b866440ceeb167688d436b1286b065fc8c07755
user <50930731+mirasac@users.noreply.github.com>
2023-10-11 09:41:08 +0200

    New exercise

---
+++ 2023-10-11/ex1.f90
@@ -0,0 +1,19 @@
+PROGRAM ex1
+IMPLICIT NONE
+! Pseudocode:
+! 1. Ask value of eccentricity.
+! 2. Given an angle value, evaluate distance.
+! 3. Print values of angle and distance.
+! 4. If angle is less or equal to one turn, return to point 2.
+REAL, PARAMETER :: p = 1.2e6 ! m
+REAL, PARAMETER :: PI = 3.14159265
+REAL :: e, r
+INTEGER :: theta
+WRITE (*,*) 'Insert eccentricity e: '
+READ (*,*) e
+WRITE (*,*) 'epsilon: ', e
+DO theta = 0, 259, 1
+    r = p / (1.0 - e * COS(theta / 180.0 * PI))
+    WRITE (*,*) 'theta: ', theta, ' r: ', r
+END DO
+END PROGRAM ex1

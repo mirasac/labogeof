cd65500424e10b1aeeacb5771e6b6340cd16deed
user <50930731+mirasac@users.noreply.github.com>
2023-11-07 13:55:05 +0100

    Improve subroutine to sanitize filename

--- utilities.f90
+++ utilities.f90
@@ -1,25 +1,31 @@
 MODULE utilities
 IMPLICIT NONE
+PUBLIC
+INTEGER, PARAMETER :: PATH_MAX = 4096  ! Maximum number of bytes in absolute paths.
+INTEGER, PARAMETER :: CHAR_MAX = 80  ! Maximum number of characters displayed in terminal.
 CONTAINS
 
 ! Subroutine to sanitize the specified filename.
+! OUT parameters:
+!   filename
+! IN optional parameters:
+!   description
 SUBROUTINE sanitize_filename(filename, description)
 ! Parameters declaration.
-CHARACTER(LEN=128), INTENT(OUT) :: filename
+CHARACTER(LEN=*), INTENT(OUT) :: filename
 CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: description
 ! Variables declaration.
-CHARACTER(LEN=80) :: string_insert
+CHARACTER(LEN=5) :: string_write
 LOGICAL :: file_exists
-! Build insert prompt.
-string_insert = 'Insert filename'
+! Build insert request message.
 IF (PRESENT(description)) THEN
-    string_insert = TRIM(string_insert) // ' for ' // TRIM(description) // ':'
+    string_write = ' for '
 ELSE
-    string_insert = TRIM(string_insert) // ':'
+    string_write = ''
 END IF
-! Loop insert prompt.
+! Loop insert request.
 DO
-    WRITE(*, '(A)') string_insert
+    WRITE(*, '(4A)') 'Insert filename', string_write, TRIM(description), ':'
     READ(*, '(A)') filename
     filename = TRIM(filename)
     INQUIRE(FILE=filename, EXIST=file_exists)

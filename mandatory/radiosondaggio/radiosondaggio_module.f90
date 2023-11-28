45f8d150492991dad7965343a2e98fba21ac733f
user <50930731+mirasac@users.noreply.github.com>
2023-11-28 10:52:53 +0100

    Add function to convert character to real

--- mandatory/radiosondaggio/radiosondaggio_module.f90
+++ mandatory/radiosondaggio/radiosondaggio_module.f90
@@ -29,6 +29,41 @@ REAL(KIND=WK) FUNCTION get_altitude(T, p_1, p_2, z_1) RESULT(z_2)
     z_2 = z_1 + R / g * (T + zero_celsius) * LOG(p_1 / p_2)
 END FUNCTION
 
+! Convert real number from character representation to numeric value.
+! IN arguments:
+!   str
+!     Character, input string containing the character representation of
+!     the number to convert.
+! IN optional arguments:
+!   sub_start
+!     Character, substring of the input string starting from which the
+!     number is written, default the conversion start from the beggining
+!     of the input string.
+!   sub_end
+!     Character, substring of the input string which follows the number
+!     to convert in the input string, default the conversion ends at the
+!     end of the input string.
+REAL(KIND=WK) FUNCTION character2real(str, sub_start, sub_end)
+    ! Dummy arguments declaration.
+    CHARACTER(LEN=*), INTENT(IN) :: str
+    CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: sub_start
+    CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: sub_end
+    ! Variables declaration.
+    INTEGER :: i_start, i_end
+    ! Set default values.
+    IF (PRESENT(sub_start)) THEN
+        i_start = INDEX(str, sub_start) + LEN(sub_start)
+    ELSE
+        i_start = 1
+    END IF
+    IF (PRESENT(sub_end)) THEN
+        i_end = INDEX(str, sub_end) - 1
+    ELSE
+        i_end = LEN(str)
+    END IF
+    READ(str(i_start : i_end), *) character2real
+END FUNCTION
+
 ! Add altitude values to and using data from the specified unit.
 ! IN arguments:
 !   unit_input
@@ -44,20 +79,14 @@ SUBROUTINE add_altitude(unit_input, unit_output)
     REAL(KIND=WK) :: h0  ! m
     REAL(KIND=WK) :: p0  ! mbar
     REAL(KIND=WK) :: T0  ! °C
-    INTEGER :: i_start, i_end
     ! Read and write header.
     READ(unit_input, '(A)') header
     WRITE(unit_output, '(A)') header
     ! Extract data from header.
-    i_start = INDEX(header, 'h0=') + 3
-    i_end = INDEX(header, 'p0=') - 1
-    READ(header(i_start : i_end), *) h0
-    i_start = INDEX(header, 'p0=') + 3
-    i_end = INDEX(header, 'T0=') - 1
-    READ(header(i_start : i_end), *) p0
-    i_start = INDEX(header, 'T0=') + 3
-    i_end = LEN(header)
-    READ(header(i_start : i_end), *) T0
+    h0 = character2real(header, sub_start='h0=', sub_end='p0=')
+    p0 = character2real(header, sub_start='p0=', sub_end='T0=')
+    T0 = character2real(header, sub_start='T0=')
+    WRITE(*, *) h0, p0, T0 ! MC debug.
     ! MC continue.
 END SUBROUTINE
 

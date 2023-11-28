d50db254e3d7ceb310e9b82dff39bfd5337e3606
user <50930731+mirasac@users.noreply.github.com>
2023-11-28 11:36:29 +0100

    Complete first request fourth mandatory exercise

--- mandatory/radiosondaggio/radiosondaggio.f90
+++ mandatory/radiosondaggio/radiosondaggio.f90
@@ -1,17 +1,17 @@
 PROGRAM radiosondaggio
 USE utilities, ONLY : PATH_MAX, WK => SP, get_filename, count_lines
-USE radiosondaggio_module, ONLY : get_altitude, add_altitude
+USE radiosondaggio_module, ONLY : get_altitude, get_pressure, add_altitude
 IMPLICIT NONE
 ! Declare variables.
 CHARACTER(LEN=10), PARAMETER :: FILENAME_CONFIG = 'config.nml'
-INTEGER, PARAMETER :: UNIT_A_INPUT = 31
-INTEGER, PARAMETER :: UNIT_A_OUTPUT = 32
+INTEGER, PARAMETER :: UNIT_INPUT = 30
+INTEGER, PARAMETER :: UNIT_OUTPUT = 31
 CHARACTER(LEN=PATH_MAX) :: filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
 NAMELIST /namelist_config/ filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
-INTEGER :: iostat_config, iostat_A_input, iostat_B_input, iostat_A_output, iostat_B_output, iostat_output
+INTEGER :: iostat_input, iostat_output
 ! Read configurations.
-OPEN(UNIT=UNIT_A_INPUT, FILE=FILENAME_CONFIG, IOSTAT=iostat_config, ACTION='READ', STATUS='OLD')
-IF (iostat_config /= 0) THEN
+OPEN(UNIT=UNIT_INPUT, FILE=FILENAME_CONFIG, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
+IF (iostat_input /= 0) THEN
     WRITE(*, 100) FILENAME_CONFIG
     CALL get_filename(filename_A_input, 'station A input', 'READ')
     CALL get_filename(filename_B_input, 'station B input', 'READ')
@@ -19,21 +19,33 @@ IF (iostat_config /= 0) THEN
     CALL get_filename(filename_B_output, 'station B output', 'WRITE')
     CALL get_filename(filename_output, 'pressure difference values', 'WRITE')
 ELSE
-    READ(UNIT_A_INPUT, NML=namelist_config)
+    READ(UNIT_INPUT, NML=namelist_config)
 END IF
-CLOSE(UNIT_A_INPUT)
+CLOSE(UNIT_INPUT)
 ! Operate on station A data.
-OPEN(UNIT=UNIT_A_INPUT, FILE=filename_A_input, IOSTAT=iostat_A_input, ACTION='READ', STATUS='OLD')
-OPEN(UNIT=UNIT_A_OUTPUT, FILE=filename_A_output, IOSTAT=iostat_A_output, ACTION='WRITE', STATUS='REPLACE')
-IF (iostat_A_input /= 0) THEN
+OPEN(UNIT=UNIT_INPUT, FILE=filename_A_input, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
+OPEN(UNIT=UNIT_OUTPUT, FILE=filename_A_output, IOSTAT=iostat_output, ACTION='WRITE', STATUS='REPLACE')
+IF (iostat_input /= 0) THEN
     WRITE(*, 100) filename_A_input
-ELSE IF (iostat_A_output /= 0) THEN
+ELSE IF (iostat_output /= 0) THEN
     WRITE(*, 100) filename_A_output
 ELSE
-    CALL add_altitude(UNIT_A_INPUT, UNIT_A_OUTPUT)
+    CALL add_altitude(UNIT_INPUT, UNIT_OUTPUT)
 END IF
-CLOSE(UNIT_A_INPUT)
-CLOSE(UNIT_A_OUTPUT)
+CLOSE(UNIT_INPUT)
+CLOSE(UNIT_OUTPUT)
+! Operate on station B data.
+OPEN(UNIT=UNIT_INPUT, FILE=filename_B_input, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
+OPEN(UNIT=UNIT_OUTPUT, FILE=filename_B_output, IOSTAT=iostat_output, ACTION='WRITE', STATUS='REPLACE')
+IF (iostat_input /= 0) THEN
+    WRITE(*, 100) filename_B_input
+ELSE IF (iostat_output /= 0) THEN
+    WRITE(*, 100) filename_B_output
+ELSE
+    CALL add_altitude(UNIT_INPUT, UNIT_OUTPUT)
+END IF
+CLOSE(UNIT_INPUT)
+CLOSE(UNIT_OUTPUT)
 ! MC continue.
 STOP
 100 FORMAT('Error opening file ', A)
--- mandatory/radiosondaggio/radiosondaggio_module.f90
+++ mandatory/radiosondaggio/radiosondaggio_module.f90
@@ -3,7 +3,7 @@ USE utilities, ONLY : WK => SP, character2real
 IMPLICIT NONE
 PRIVATE
 PUBLIC :: R, g, zero_celsius
-PUBLIC :: get_altitude, add_altitude
+PUBLIC :: get_altitude, get_pressure, add_altitude
 REAL(KIND=WK), PARAMETER :: R = 287.0_WK  ! J / (Kg K)
 REAL(KIND=WK), PARAMETER :: g = 9.81_WK  ! m / s^2
 REAL(KIND=WK), PARAMETER :: zero_celsius = 273.15  ! K
@@ -27,6 +27,26 @@ REAL(KIND=WK) FUNCTION get_altitude(T, p_1, p_2, z_1) RESULT(z_2)
     REAL(KIND=WK), INTENT(IN) :: z_1  ! m
     ! Compute result.
     z_2 = z_1 + R / g * (T + zero_celsius) * LOG(p_1 / p_2)
+END FUNCTION
+
+! Evaluate pressure in mbar of final point using barometric formula.
+! IN arguments:
+!   T
+!     Scalar real, temperature in °C.
+!   z_1
+!     Scalar real, altitude in m of initial point.
+!   z_2
+!     Scalar real, altitude in m of final point.
+!   p_1
+!     Scalar real, pressure in mbar of initial point.
+REAL(KIND=WK) FUNCTION get_pressure(T, z_1, z_2, p_1) RESULT(p_2)
+    ! Dummy arguments declaration.
+    REAL(KIND=WK), INTENT(IN) :: T  ! °C
+    REAL(KIND=WK), INTENT(IN) :: z_1  ! m
+    REAL(KIND=WK), INTENT(IN) :: z_2  ! m
+    REAL(KIND=WK), INTENT(IN) :: p_1  ! mbar
+    ! Compute result.
+    p_2 = p_1 * EXP(- (z_2 - z_1) * g / (R * (T + zero_celsius)))
 END FUNCTION
 
 ! Add altitude values to and using data from the specified unit.
@@ -60,10 +80,10 @@ SUBROUTINE add_altitude(unit_input, unit_output)
         READ(unit_input, *, IOSTAT=iostat_input) p_2, T_2
         IF (iostat_input < 0) EXIT
         z_2 = get_altitude((T_1 + T_2) / 2.0_WK, p_1, p_2, z_1)
+        WRITE(unit_output, *) p_2, T_2, z_2
         p_1 = p_2
         T_1 = T_2
         z_1 = z_2
-        WRITE(unit_output, *) p_2, T_2, z_2
     END DO
 END SUBROUTINE
 

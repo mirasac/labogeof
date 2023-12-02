07e319349053b5b109e9f1b0b26d1984e31b0627
user <50930731+mirasac@users.noreply.github.com>
2023-12-02 18:03:45 +0100

    Create grid fourth mandatory exercise

--- mandatory/radiosondaggio/radiosondaggio.f90
+++ mandatory/radiosondaggio/radiosondaggio.f90
@@ -9,10 +9,12 @@ INTEGER, PARAMETER :: UNIT_INPUT = 30
 INTEGER, PARAMETER :: UNIT_OUTPUT = 31
 CHARACTER(LEN=PATH_MAX) :: filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
 NAMELIST /namelist_config/ filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
-INTEGER :: iostat_input, iostat_output, n_lines, stat_p, stat_T, stat_z, stat_z_grid, n_layers
-REAL(KIND=WK), ALLOCATABLE, DIMENSION(:) :: p, p_A, p_B  ! mbar
+INTEGER :: iostat_input, iostat_output, n_lines, stat_p, stat_T, stat_z, stat_z_A, stat_p_A, n_layers, i_layer, i_line, i_tmp
+REAL(KIND=WK), ALLOCATABLE :: p(:), p_A(:)  ! mbar
 REAL(KIND=WK), ALLOCATABLE :: T(:)  ! °C
-REAL(KIND=WK), ALLOCATABLE :: z(:), z_grid(:)  ! m
+REAL(KIND=WK) :: T_layer  ! °C
+REAL(KIND=WK), ALLOCATABLE :: z(:), z_A(:)  ! m
+REAL(KIND=WK) :: z_layer, z_0  ! m
 ! Read configurations.
 OPEN(UNIT=UNIT_INPUT, FILE=FILENAME_CONFIG, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
 IF (iostat_input /= 0) THEN
@@ -38,25 +40,42 @@ ELSE
     ALLOCATE(p(n_lines), STAT=stat_p)
     ALLOCATE(T(n_lines), STAT=stat_T)
     ALLOCATE(z(n_lines), STAT=stat_z)
-    ALLOCATE(z_grid(n_lines), STAT=stat_z_grid)
     IF (stat_p > 0) THEN
         WRITE(*, 101) 'station A pressure'
     ELSE IF (stat_T > 0) THEN
         WRITE(*, 101) 'station A temperature'
     ELSE IF (stat_z > 0) THEN
         WRITE(*, 101) 'station A altitude'
-    ELSE IF (stat_z_grid > 0) THEN
-        WRITE(*, 101) 'station A vertical grid'
     ELSE
         ! Add altitude values.
         CALL add_altitude(UNIT_INPUT, UNIT_OUTPUT, p=p, T=T, z=z)
         ! Create pressure analyses.
-        z_grid(1) = (INT(z(1) / z_res) + 1) * z_res
-        z_grid(n_lines) = INT(z(n_lines) / z_res) * z_res
         n_layers = INT((z(n_lines) - z(1)) / z_res) + 1
-        WRITE(*, *) z(1), z_grid(1), z(n_lines), z_grid(n_lines) ! MC debug.
-        WRITE(*, *) INT((z_grid(n_lines) - z_grid(1)) / z_res) + 1, n_layers ! MC debug.
-        ! MC continue.
+        ALLOCATE(z_A(n_layers), STAT=stat_z_A)
+        ALLOCATE(p_A(n_layers), STAT=stat_p_A)
+        IF (stat_z_A > 0) THEN
+            WRITE(*, 101) 'station A grid altitude'
+        ELSE IF (stat_p_A > 0) THEN
+            WRITE(*, 101) 'station A grid pressure'
+        ELSE
+            i_layer = 1
+            DO i_line = 2, n_lines, 1
+                WRITE(*, *) '---', z(i_line - 1), '---', i_line - 1 ! MC debug.
+                T_layer = (T(i_line) + T(i_line - 1)) / 2.0_WK
+                i_tmp = 1
+                z_0 = (INT(z(i_line - 1) / z_res)) * z_res
+                DO
+                    z_layer = z_0 + i_tmp * z_res
+                    IF (z_layer >= z(i_line)) EXIT
+                    WRITE(*, *) '   ', z_layer, '   ', i_layer ! MC debug.
+                    z_A(i_layer) = z_layer
+                    p_A(i_layer) = p(i_line) ! MC evaluate pressure.
+                    i_tmp = i_tmp + 1
+                    i_layer = i_layer + 1
+                END DO
+            END DO
+            ! MC continue.
+        END IF
     END IF
     IF (ALLOCATED(p)) THEN
         DEALLOCATE(p)
@@ -66,9 +85,6 @@ ELSE
     END IF
     IF (ALLOCATED(z)) THEN
         DEALLOCATE(z)
-    END IF
-    IF (ALLOCATED(z_grid)) THEN
-        DEALLOCATE(z_grid)
     END IF
 END IF
 CLOSE(UNIT_INPUT)
@@ -87,6 +103,12 @@ CLOSE(UNIT_INPUT)
 CLOSE(UNIT_OUTPUT)
 ! Evaluate pressure differences.
 ! MC continue, only if previous operations are successful.
+IF (ALLOCATED(z_A)) THEN
+    DEALLOCATE(z_A)
+END IF
+IF (ALLOCATED(p_A)) THEN
+    DEALLOCATE(p_A)
+END IF
 STOP
 100 FORMAT('Error opening file ', A)
 101 FORMAT('Error allocating memory for ', A)

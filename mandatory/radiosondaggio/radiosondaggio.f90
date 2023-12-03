16cd3e7284c8d570c73726b6ef0aba7c28ebf8b0
user <50930731+mirasac@users.noreply.github.com>
2023-12-03 11:29:45 +0100

    Complete second request

--- mandatory/radiosondaggio/radiosondaggio.f90
+++ mandatory/radiosondaggio/radiosondaggio.f90
@@ -1,5 +1,5 @@
 PROGRAM radiosondaggio
-USE utilities, ONLY : PATH_MAX, WK => SP, get_filename, count_lines
+USE utilities, ONLY : PATH_MAX, WK => SP, EPS, get_filename, count_lines
 USE radiosondaggio_module, ONLY : add_altitude, get_pressure, get_analyses
 IMPLICIT NONE
 ! Declare variables.
@@ -9,10 +9,12 @@ INTEGER, PARAMETER :: UNIT_INPUT = 30
 INTEGER, PARAMETER :: UNIT_OUTPUT = 31
 CHARACTER(LEN=PATH_MAX) :: filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
 NAMELIST /namelist_config/ filename_A_input, filename_B_input, filename_A_output, filename_B_output, filename_output
-INTEGER :: iostat_input, iostat_output, n_lines, stat_p, stat_T, stat_z, stat_z_A, stat_p_A, n_layers, stat_z_B, stat_p_B
-REAL(KIND=WK), ALLOCATABLE :: p(:), p_A(:), p_B(:)  ! mbar
+INTEGER :: iostat_input, iostat_output, n_lines, stat_p, stat_T, stat_z, &
+    stat_z_A, stat_p_A, n_A, n_B, stat_z_B, stat_p_B, i_A_min, i_B_min, i_A_max, i_B_max, i
+REAL(KIND=WK), ALLOCATABLE :: p(:), p_A(:), p_B(:), delta_p(:)  ! mbar
 REAL(KIND=WK), ALLOCATABLE :: T(:)  ! °C
 REAL(KIND=WK), ALLOCATABLE :: z(:), z_A(:), z_B(:)  ! m
+REAL(KIND=WK) :: z_min, z_max  ! m
 ! Read configurations.
 OPEN(UNIT=UNIT_INPUT, FILE=FILENAME_CONFIG, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
 IF (iostat_input /= 0) THEN
@@ -48,9 +50,9 @@ ELSE
         ! Add altitude values.
         CALL add_altitude(UNIT_INPUT, UNIT_OUTPUT, p=p, T=T, z=z)
         ! Create pressure analyses.
-        n_layers = INT((z(n_lines) - z(1)) / z_res) + 1
-        ALLOCATE(z_A(n_layers), STAT=stat_z_A)
-        ALLOCATE(p_A(n_layers), STAT=stat_p_A)
+        n_A = INT((z(n_lines) - z(1)) / z_res) + 1
+        ALLOCATE(z_A(n_A), STAT=stat_z_A)
+        ALLOCATE(p_A(n_A), STAT=stat_p_A)
         IF (stat_z_A > 0) THEN
             WRITE(*, 101) 'station A grid altitude'
         ELSE IF (stat_p_A > 0) THEN
@@ -71,6 +73,7 @@ ELSE
 END IF
 CLOSE(UNIT_INPUT)
 CLOSE(UNIT_OUTPUT)
+WRITE(*, *) ! MC debug.
 ! Operate on station B data.
 OPEN(UNIT=UNIT_INPUT, FILE=filename_B_input, IOSTAT=iostat_input, ACTION='READ', STATUS='OLD')
 OPEN(UNIT=UNIT_OUTPUT, FILE=filename_B_output, IOSTAT=iostat_output, ACTION='WRITE', STATUS='REPLACE')
@@ -93,9 +96,9 @@ ELSE
         ! Add altitude values.
         CALL add_altitude(UNIT_INPUT, UNIT_OUTPUT, p=p, T=T, z=z)
         ! Create pressure analyses.
-        n_layers = INT((z(n_lines) - z(1)) / z_res) + 1
-        ALLOCATE(z_B(n_layers), STAT=stat_z_B)
-        ALLOCATE(p_B(n_layers), STAT=stat_p_B)
+        n_B = INT((z(n_lines) - z(1)) / z_res) + 1
+        ALLOCATE(z_B(n_B), STAT=stat_z_B)
+        ALLOCATE(p_B(n_B), STAT=stat_p_B)
         IF (stat_z_B > 0) THEN
             WRITE(*, 101) 'station A grid altitude'
         ELSE IF (stat_p_B > 0) THEN
@@ -117,7 +120,29 @@ END IF
 CLOSE(UNIT_INPUT)
 CLOSE(UNIT_OUTPUT)
 ! Evaluate pressure differences.
-! MC continue, only if previous operations are successful.
+OPEN(UNIT=UNIT_OUTPUT, FILE=filename_output, IOSTAT=iostat_output, ACTION='WRITE', STATUS='REPLACE')
+IF (iostat_output /= 0) THEN
+    WRITE(*, 100) filename_output
+ELSE IF (stat_z_A > 0 .OR. stat_p_A > 0 .OR. stat_z_B > 0 .OR. stat_p_B > 0) THEN
+    WRITE(*, *) 'Pressure differences not evaluated'
+ELSE
+    ! Find index of minimum common altitude.
+    z_min = MAX(z_A(1), z_B(1))
+    i_A_min = MINLOC(z_A, 1, ABS(z_A - z_min) <= EPS)
+    i_B_min = MINLOC(z_B, 1, ABS(z_B - z_min) <= EPS)
+    WRITE(*, *) z_min, i_A_min, i_B_min ! MC debug.
+    ! Find index of maximum common altitude.
+    z_max = MIN(z_A(n_A), z_B(n_B))
+    i_A_max = MAXLOC(z_A, 1, ABS(z_A - z_max) <= EPS)
+    i_B_max = MAXLOC(z_B, 1, ABS(z_B - z_max) <= EPS)
+    WRITE(*, *) z_max, i_A_max, i_B_max ! MC debug.
+    ! Write pressure differences.
+    delta_p =  p_A(i_A_min : i_A_max) - p_B(i_B_min : i_B_max)
+    DO i = 1, SIZE(delta_p), 1
+        WRITE(UNIT_OUTPUT, *) z_A(i + i_A_min - 1), delta_p(i)
+    END DO
+END IF
+CLOSE(UNIT_OUTPUT)
 IF (ALLOCATED(z_A)) THEN
     DEALLOCATE(z_A)
 END IF
@@ -129,6 +154,9 @@ IF (ALLOCATED(z_B)) THEN
 END IF
 IF (ALLOCATED(p_B)) THEN
     DEALLOCATE(p_B)
+END IF
+IF (ALLOCATED(delta_p)) THEN
+    DEALLOCATE(delta_p)
 END IF
 STOP
 100 FORMAT('Error opening file ', A)
